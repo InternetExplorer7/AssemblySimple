b564fa0f85ae6f442a6ff5f19f0b4eacf7d396da
Kaveh Khorram <kavehkhorram@orton-wired-10-100-32-230.resnet.wsu.edu>
2017-10-10 20:39:29 -0700

    first

---
+++ sample.asm
@@ -0,0 +1,68 @@
+
+.data
+    firstNumberPrompt:  .asciiz "Enter the first number"
+    secondNumberPrompt: .asciiz "\nEnter the second number"
+    thirdOperationPrompt: .asciiz "\nEnter the operation type"
+    result: .asciiz "\nThe result is: "
+.text
+main:
+    # Prompt the user to enter first number
+    li $v0, 4
+    la $a0, firstNumberPrompt
+    syscall
+
+    # Get the first number.
+    li $v0, 5
+    syscall
+
+    # store the result in $t0.
+    move $t0, $v0
+
+    # Prompt the user to enter the second number
+    li $v0, 4
+    la $a0, secondNumberPrompt
+    syscall
+
+    # Get the second number
+    li $v0, 5
+    syscall
+
+    # store the result in $t1
+    move $t1, $v0
+
+    # Prompt the user for the operation type
+    li $v0, 4
+    la $a0, thirdOperationPrompt
+    syscall
+
+    # Get the operation type
+    li $v0, 5
+    syscall
+
+    # Store the result in $t2
+    move $t2, $v0
+
+    # Where the meat of the code will go.
+    addi $t8, $zero, 0 # add
+    addi $t9, $zero, 1 # sub
+    beq $t8, $t2, Add
+    beq $t9, $t2, Sub
+    j Mul
+
+    Add:
+    add $s0, $t0, $t1
+    j PRINT_THEN_EXIT
+    Sub:
+    sub $s0, $t0, $t1
+    j PRINT_THEN_EXIT
+    Mul:
+    mul $s0, $t0, $t1
+    j PRINT_THEN_EXIT
+    
+    PRINT_THEN_EXIT:
+    li $v0, 1
+    move $a0, $s0
+    syscall
+    # EXIT
+    li $v0, 10
+    syscall
